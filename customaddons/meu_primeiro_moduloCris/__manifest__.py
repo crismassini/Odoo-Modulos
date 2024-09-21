0e009dd8d50eea0ab1212ab154e4a6a55c92b90c
Cristhian Massini <60633624+crismassini@users.noreply.github.com>
2024-09-21 12:07:55 -0300

    Commit Inicial

---
+++ customaddons/meu_primeiro_moduloCris/__manifest__.py
@@ -0,0 +1,7 @@
+{
+    'name': 'Financeiro - ADO',
+    'version': '1.0',
+    'description': ' Módulo da aula de odoo',
+    'summary': 'Módulo de teste',
+    'author': 'RGM Tecnologia'
+}
---
+++ customaddons/meu_primeiro_moduloCris/models/meu_primeiro_modelocris.py
@@ -0,0 +1,8 @@
+from odoo import models
+
+class MeuPrimeiroModeloCris(models.Model):
+    _name = 'meu.primeiro.modelocris'
+    _description = 'Nosso primeiro modelo feito no Odoo'
+
+
+
